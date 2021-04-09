f6f67dc8eeec9c04c989bdf34d438b8367a4b9d0
Emily Santos <emily.mimy12@gmail.com>
2021-04-09 12:20:44 -0300

    Função quase final

--- main.rb
+++ main.rb
@@ -1,5 +1,14 @@
+def verificarPalavra(palavra)
+    novaPalavra = palavra.reverse
+
+    puts novaPalavra == palavra ? "A palavra é um palindrome\n#{novaPalavra}" : "A palavra não é um palindrome"
+
+end
+
 puts "Digite uma palavra:"
 
-palavra = gets.chomp() #gets.chomp faz com que seja pego o valor escrito sem o \n
+palavra.uppercase = gets.chomp #gets.chomp faz com que seja pego o valor escrito sem o \n
 
 puts "A palavra foi #{palavra}" if !palavra.empty?
+
+verificarPalavra(palavra)
