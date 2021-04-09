b8abfbacd630a90fd8854d9146c3f6e3ee34089a
Emily Santos <emily.mimy12@gmail.com>
2021-04-09 14:33:49 -0300

    Repetição de entradas

--- main.rb
+++ main.rb
@@ -3,14 +3,27 @@
 def verificarPalavra(palavra)
     novaPalavra = palavra.reverse
 
-    puts novaPalavra == palavra ? "A palavra é um palindrome\n#{novaPalavra}" : "A palavra não é um palindrome\n#{novaPalavra}"
+    novaPalavra == palavra ? (puts "A palavra é um palindrome\n#{palavra} - #{novaPalavra}") : (puts "A palavra não é um palindrome\n#{palavra} - #{novaPalavra}")
 
 end
 
-puts "Digite uma palavra:"
+i = 0
+while i == 0
+    puts "Digite uma palavra:"
+    palavra = gets.chomp.upcase #gets.chomp faz com que seja pego o valor escrito sem o \n
 
-palavra = gets.chomp.upcase #gets.chomp faz com que seja pego o valor escrito sem o \n
+    case palavra.empty? #switch case
+    when false           
+        #puts "A palavra foi #{palavra}" if !palavra.empty?        
+        verificarPalavra(palavra)
+        puts "-" * 10
+    else
+        #puts "Palavra vazia"
+        i = 1
+    end
+end
 
-#puts "A palavra foi #{palavra}" if !palavra.empty?
 
-verificarPalavra(palavra)
+
+
+
